91943f5fedc728c1253cb10a061f01e3f6830c0e
nnlgsakib <nlgarts@outlook.com>
2024-05-01 15:37:19 +0600

    ...

--- example/wm2m.js
+++ example/wm2m.js
@@ -2,7 +2,7 @@ const {convertWmindTomind} = require('msc-js');
 
 
 async function wm2m (){
-    let privatekey = '0x0c7dee41c5d202fa97708a35afcdb5b2f8e65d0d968f7a882a0263463ccc12d6';
+    let privatekey = '';
 let amount = 2;
     try{
         const txn = await convertWmindTomind(privatekey , amount) ; return
